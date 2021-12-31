1c3ae915e52fa8bc87a4f87941f005e611428042
Rekord <1324596506@qq.com>
2021-12-31 14:59:06 +0800

    add the table of bill

--- handbook.sql
+++ handbook.sql
@@ -13,3 +13,17 @@ create table if not exists user(
 -- insert user(`username`,`password`) values("admin","123456");
 
 -- select * from user;
+
+-- 删除bill表
+-- drop table if exists bill;
+-- 创建bill表
+create table if not exists bill(
+`id` INT(11) auto_increment primary key,
+`money` double not null,
+`label` varchar(40) not null,
+`comment` varchar(400) not null,
+`calendar` varchar(100) not null,
+`ctgr` enum("1","0") not null
+)ENGINE=InnoDB DEFAULT charset=utf8;
+
+-- select * from bill;
