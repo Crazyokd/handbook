be116e0237fd34e60a5bfe7b5a4288985dcf5723
Rekord <1324596506@qq.com>
2022-01-01 12:34:46 +0800

    modify User

--- backend/pkg/handbook/domain/User.java
+++ backend/pkg/handbook/domain/User.java
@@ -2,17 +2,10 @@ package pkg.handbook.domain;
 
 public class User {
 
-	private String id;
 	private String username;
 	private String password;
 	
     
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
 	public String getUsername() {
 		return username;
 	}
