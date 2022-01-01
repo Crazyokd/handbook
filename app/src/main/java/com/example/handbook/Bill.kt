e0ff99ebc9db556fcd02e1e5b8e61dae63ee973e
Rekord <1324596506@qq.com>
2022-01-01 15:22:33 +0800

    modidy Bill.kt

--- app/src/main/java/com/example/handbook/Bill.kt
+++ app/src/main/java/com/example/handbook/Bill.kt
@@ -4,10 +4,16 @@ import java.io.Serializable
 import java.sql.Time
 import java.util.*
 
-class Bill(val money:Double?,val label:String,var comment:String?,val calendar: String?,val ctgr:Int):Serializable{
+class Bill(
+    val money: Double?,
+    val label: String,
+    var comment: String?,
+    val calendar: String?,
+    val ctgr: Int
+) : Serializable {
 
-    companion object{
-        const val TYPE_INCOME=1
-        const val TYPE_EXPENSE=0
+    companion object {
+        const val TYPE_INCOME = 1
+        const val TYPE_EXPENSE = 0
     }
 }
