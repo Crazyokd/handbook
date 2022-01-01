d37932d091ee8a476cc7bd3e4c195acb3dcde777
Rekord <1324596506@qq.com>
2022-01-01 12:37:20 +0800

    修改数据库设计

--- handbook.sql
+++ handbook.sql
@@ -23,7 +23,16 @@ create table if not exists bill(
 `label` varchar(40) not null,
 `comment` varchar(400) not null,
 `calendar` varchar(100) not null,
-`ctgr` enum("1","0") not null
+`ctgr` enum("1","0") not null,
+`user_id` int not null,
+constraint bill_user_id foreign key(`user_id`) references user(`id`)
 )ENGINE=InnoDB DEFAULT charset=utf8;
 
 -- select * from bill;
+
+-- insert into bill(`money`,`label`,`comment`,`calendar`,`ctgr`,`user_id`) values(5600,"工资薪水", "salary for 12", "2021.12.31","1",1);
+-- delete from bill where user_id `user_id` = 
+-- select `money`,`label`,`comment`,`calendar`,`ctgr` from bill where user_id = 1;
+
+
+
