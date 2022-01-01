5f78d430e76b1c2dea7f17a23d11852dc02c0766
Rekord <1324596506@qq.com>
2022-01-01 16:12:55 +0800

    modify the table of bill

--- handbook.sql
+++ handbook.sql
@@ -5,8 +5,7 @@ use handbook;
 -- drop table if exists user;
 -- 创建user表
 create table if not exists user(
-`id` INT(11) auto_increment primary key,
-`username` varchar(40) not null unique,
+`username` varchar(40) primary key,
 `password` varchar(40) not null
 )ENGINE=InnoDB DEFAULT charset=utf8;
 
@@ -23,15 +22,15 @@ create table if not exists bill(
 `label` varchar(40) not null,
 `comment` varchar(400) not null,
 `calendar` varchar(100) not null,
-`ctgr` enum("1","0") not null,
-`user_id` int not null,
-constraint bill_user_id foreign key(`user_id`) references user(`id`)
+`ctgr` int not null,
+`user_name` varchar(40) not null,
+constraint bill_user_name foreign key(`user_name`) references user(`username`)
 )ENGINE=InnoDB DEFAULT charset=utf8;
 
 -- select * from bill;
 
--- insert into bill(`money`,`label`,`comment`,`calendar`,`ctgr`,`user_id`) values(5600,"工资薪水", "salary for 12", "2021.12.31","1",1);
--- delete from bill where user_id `user_id` = 
+-- insert into bill(`money`,`label`,`comment`,`calendar`,`ctgr`,`user_name`) values(5600,"工资薪水", "salary for 12", "2021.12.31","1","admin");
+-- delete from bill where user_name `user_id` = 
 -- select `money`,`label`,`comment`,`calendar`,`ctgr` from bill where user_id = 1;
 
 
